46450fac1ee06e33a10d04251494132482ef8be7
hasanmehedi9034 <mehedi90344@gmail.com>
2023-01-26 00:12:45 +0600

    express function

--- index.js
+++ index.js
@@ -2,11 +2,23 @@ const express = require('express');
 
 const app = express();
 
+// app.use(express.json());
+// app.use(express.raw());
+// app.use(express.text());
+// app.use(express.urlencoded());
+// app.use(express.static(`${__dirname}/public`, {
+//     index: 'home.html'
+// }))
+app.use()
+
+
 app.get('/', (req, res) => {
+    
     res.send('this is home page')
 })
 
 app.post('/', (req, res) => {
+    console.log(req.body);
     res.send('this is home page with  post request');
 })
 
